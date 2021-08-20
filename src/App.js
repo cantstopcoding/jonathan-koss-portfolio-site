0a0238b87422dac1790e845ac64bd263597915b5
cantstopcoding <jonathan_koss@yahoo.com>
2021-08-20 12:30:34 -0400

    import About, Contact, Home and NoMatch components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Contact } from "./Contact";
+import { NoMatch } from "./NoMatch";
 
 class App extends Component {
   render() {
@@ -8,7 +12,7 @@ class App extends Component {
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="about" component={About} />
+            <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route component={NoMatch} />
           </Switch>
