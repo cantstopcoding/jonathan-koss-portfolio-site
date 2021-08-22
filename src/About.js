387038be30133954e2ad12132e92875723b362bb
cantstopcoding <jonathan_koss@yahoo.com>
2021-08-22 13:18:21 -0400

    delete excess files

--- src/About.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const About = () => (
-  <div>
-    <h2>Jonathan Koss</h2>
-    <p>My About Page!</p>
-  </div>
-);
--- src/NoMatch.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const NoMatch = () => (
-  <div>
-    <p>No Match </p>
-  </div>
-);
