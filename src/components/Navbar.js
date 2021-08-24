5678dea0b123454abb6a350abaa5726cb32cedbc
cantstopcoding <jonathan_koss@yahoo.com>
2021-08-24 19:19:44 -0400

    add linke and menu button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav>
+      <Link to="/" className="pl-8">
+        About
+      </Link>
+      <svg
+        className="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+    </nav>
   );
 };
 
