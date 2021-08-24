b76ded2af747603589812c0ccc535cc32aa84ed9
cantstopcoding <jonathan_koss@yahoo.com>
2021-08-24 19:18:59 -0400

    in app.js add navbar component and comment ount unecessary code for now in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "./Home";
-import { About } from "./About";
-import { Contact } from "./Contact";
-import { NoMatch } from "./NoMatch";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { Home } from "./pages/Home";
+// import { About } from "./pages/About";
+// import { Contact } from "./pages/Contact";
+// import { NoMatch } from "./pages/NoMatch";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <>
         <Router>
-          <Switch>
+          <Navbar />
+          {/* <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route component={NoMatch} />
-          </Switch>
+          </Switch> */}
         </Router>
-      </React.Fragment>
+      </>
     );
   }
 }
