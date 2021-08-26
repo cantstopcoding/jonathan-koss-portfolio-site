d67036d4a245f5d860f6a3589b4d8dbc0e7f9c99
cantstopcoding <jonathan_koss@yahoo.com>
2021-08-25 20:24:11 -0400

    abstract apply content to index.css

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,13 +3,13 @@ import ImageOne from "../images/selfie.jpeg";
 
 const Content = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+    <div className="selfie-card">
       <img
         src={ImageOne}
         alt="selfie"
         className="h-full rounded mb-20 shadow"
       />
-      <div className="flex flex-col justify-center items-center">
+      <div className="center-content">
         <h2 className="text-2xl mb-2">Jonathan Koss</h2>
       </div>
     </div>
