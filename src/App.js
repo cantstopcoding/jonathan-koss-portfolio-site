19706a3f7725e898a0999d11351e4918d8de5d06
cantstopcoding <jonathan_koss@yahoo.com>
2021-08-29 09:43:05 -0400

    make App component a functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
@@ -9,29 +9,27 @@ import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Dropdown from "./components/Dropdown";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Router>
-          <Navbar />
-          <Dropdown />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Dropdown />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
 
-          {/* <Switch>
+        {/* <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route component={NoMatch} />
           </Switch> */}
-        </Router>
-      </>
-    );
-  }
-}
+      </Router>
+    </>
+  );
+};
 
 export default App;
