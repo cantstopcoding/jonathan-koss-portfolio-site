70172fef53bcb548b75e3065e811f19ac4e2785c
cantstopcoding <jonathan_koss@yahoo.com>
2021-09-20 19:45:32 -0400

    change name of import file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./tailwind.css";
+import "../src/css/tailwind.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
